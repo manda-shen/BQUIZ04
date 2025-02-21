170094567970f432b32ea059514bfc428d8f9a63
Manda <mandashen@gmail.com>
2025-02-21 09:08:19 +0800

    補充:對空的購物車進行儲存前的檢查

--- front/checkout.php
+++ front/checkout.php
@@ -1,4 +1,10 @@
 <?php
+
+/* if(empty($_SESSION['cart'])){
+    to("index.php?do=buycart&err=1");
+    exit();
+} */
+
     $user = $Mem->find(['acc' => $_SESSION["Mem"]]);
 ?>
 <h2 class="ct">填寫資料</h2>
@@ -71,6 +77,10 @@
         }
         $.post("./api/checkout.php",data,function(res){
             // console.log(res);
+            if(res=='1'){
+            alert("購物車尚無商品，不需結帳");
+            return;
+        }
             alert("訂購成功\n感謝您的選購");
             location.href='?do=main';
 
